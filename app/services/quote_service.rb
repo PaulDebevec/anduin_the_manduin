c30034eb2bbd0c8775d1bd0d1fa0404e09741ea0
Paul Debevec <paulmdebevec@gmail.com>
2021-03-01 12:49:46 -0700

    Update quote API. Switch to active API

--- app/services/quote_service.rb
+++ app/services/quote_service.rb
@@ -1,12 +1,12 @@
 class QuoteService
   def self.get_quote
-    request = conn.get('/quote')
-    quote_obj = JSON.parse(request.body, symbolize_names: true)
+    response = conn.get('/random')
+    quote_obj = JSON.parse(response.body, symbolize_names: true)
     @quote = Quote.new(quote_obj)
   end
 
   private
   def self.conn
-    Faraday.new('https://jam-session-ms.herokuapp.com')
+    Faraday.new('http://api.quotable.io')
   end
 end
